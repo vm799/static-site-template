3623044da9bc40e426da3503d0fdca7e5c004bbf
vm799 <89162649+vm799@users.noreply.github.com>
2022-01-19 14:32:42 +0000

    animating overlay

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const btnHamburger = document.getElementById("btnHamburger")
 
+const headerMenu= document.querySelector(".header")
+
+const overlay=document.querySelector(".overlay")
+
 btnHamburger.addEventListener("click", function(){
-   if(btnHamburger.classList.contains("open")){
-    btnHamburger.classList.remove("open")
+   if(headerMenu.classList.contains("open")){
+   headerMenu.classList.remove("open")
+   overlay.classList.remove("fade-in")
+   overlay.classList.add("fade-out")
    }else {
-       btnHamburger.classList.add("open")
+       headerMenu.classList.add("open")
+       overlay.classList.remove("fade-out")
+       overlay.classList.add("fade-in")
+       
    }
 })
