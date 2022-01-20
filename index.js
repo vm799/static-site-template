39e6e1f126f879b9d66e69d60c2657e8b8bec6e7
vm799 <89162649+vm799@users.noreply.github.com>
2022-01-20 08:34:51 +0000

    js functionality of fade-in overlay and menu

--- index.js
+++ index.js
@@ -4,15 +4,23 @@ const headerMenu= document.querySelector(".header")
 
 const overlay=document.querySelector(".overlay")
 
+const fadeEls= document.querySelectorAll(".has-fade")
+
 btnHamburger.addEventListener("click", function(){
    if(headerMenu.classList.contains("open")){
    headerMenu.classList.remove("open")
-   overlay.classList.remove("fade-in")
-   overlay.classList.add("fade-out")
-   }else {
+
+   fadeEls.forEach(function(e){
+        e.classList.add("fade-out")
+        e.classList.remove("fade-in")
+   })
+   } else {
        headerMenu.classList.add("open")
-       overlay.classList.remove("fade-out")
-       overlay.classList.add("fade-in")
-       
-   }
+
+   fadeEls.forEach(function(e){
+        e.classList.add("fade-in")
+        e.classList.remove("fade-out")
+   })
+
+}
 })
